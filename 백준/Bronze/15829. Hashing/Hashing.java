d558c9e23a16ef070ef6da25d0cd0c2d5daa0ab0
Polar <125350978+wovlf@users.noreply.github.com>
2026-03-15 15:33:13 +0900

    [Bronze II] Title: Hashing, Time: 64 ms, Memory: 11604 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+
+class Node{
+    int end;
+    long index;
+    public Node(int end, long index){
+        this.end = end;
+        this.index = index;
+    }
+
+
+}
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        String str = br.readLine();
+        char [] ch = str.toCharArray();
+        double sum = 0;
+        int k = 0;
+        for(int i = 0;i<ch.length;i++){
+            sum = (sum  % 1234567891 ) + (((ch[i] - 96) * Math.pow(31, k)) % 1234567891) % 1234567891;
+            k++;
+        }
+        System.out.println((long)sum);
+    }
+}
